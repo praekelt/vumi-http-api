745583e1bfed72e73f16e9529ecd9101d070100b
Rudi Giesler <rudi@praekelt.com>
2015-06-24 17:00:35 +0200

    Add tests for event pushing

--- vumi_http_api/tests/test_vumi_app.py
+++ vumi_http_api/tests/test_vumi_app.py
@@ -9,7 +9,7 @@ from twisted.web.server import NOT_DONE_YET
 from urlparse import urlparse, urlunparse
 
 from vumi.application.tests.helpers import ApplicationHelper
-from vumi.message import TransportUserMessage
+from vumi.message import TransportEvent, TransportUserMessage
 from vumi.tests.helpers import VumiTestCase
 from vumi.tests.utils import LogCatcher, MockHttpServer
 from vumi.utils import HttpTimeoutError
@@ -35,6 +35,7 @@ class TestVumiApiWorkerBase(VumiTestCase):
         self.config = {
             'conversation_key': 'key_conversation',
             'push_message_url': self.get_message_url(),
+            'push_event_url': self.get_event_url(),
         }
         self.config.update(config_overrides)
         self.app = yield self.app_helper.get_application(self.config)
@@ -43,38 +44,15 @@ class TestVumiApiWorkerBase(VumiTestCase):
     def get_message_url(self):
         return self.mock_push_server.url
 
+    def get_event_url(self):
+        return self.mock_push_server.url
+
     def handle_request(self, request):
         self.push_calls.put(request)
         return NOT_DONE_YET
 
 
 class TestVumiApiWorker(TestVumiApiWorkerBase):
-
-    @inlineCallbacks
-    def test_ack_event(self):
-        yield self.start_app_worker()
-        event = self.app_helper.make_ack()
-        yield self.app_helper.dispatch_event(event)
-
-    @inlineCallbacks
-    def test_nack_event(self):
-        yield self.start_app_worker()
-        event = self.app_helper.make_nack()
-        yield self.app_helper.dispatch_event(event)
-
-    @inlineCallbacks
-    def test_unknown_event(self):
-        yield self.start_app_worker()
-        # temporarily pretend the worker doesn't know about acks
-        del self.app._event_handlers['ack']
-        event = self.app_helper.make_ack()
-        yield self.app_helper.dispatch_event(event)
-
-    @inlineCallbacks
-    def test_delivery_report(self):
-        yield self.start_app_worker()
-        event = self.app_helper.make_delivery_report()
-        yield self.app_helper.dispatch_event(event)
 
     @inlineCallbacks
     def test_post_inbound_message(self):
@@ -206,6 +184,162 @@ class TestVumiApiWorker(TestVumiApiWorkerBase):
                 'in 1', message_id='1', conv=self.conversation)
             [noconv_log] = lc.messages()
         self.assertTrue(msg['message_id'] in noconv_log)
+
+    def make_outbound(self, conv, content, **kw):
+        return self.app_helper.make_outbound(content, conv=conv, **kw)
+
+    @inlineCallbacks
+    def test_post_ack_event(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+        event_d = self.app_helper.make_dispatch_ack(
+            msg1, conv=self.conversation)
+        req = yield self.push_calls.get()
+        posted_json_data = req.content.read()
+        self.assertEqual(
+            req.requestHeaders.getRawHeaders('content-type'),
+            ['application/json; charset=utf-8'])
+        req.finish()
+        ack1 = yield event_d
+        self.assertEqual(TransportEvent.from_json(posted_json_data), ack1)
+
+    @inlineCallbacks
+    def test_post_nack_event(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+        event_d = self.app_helper.make_dispatch_nack(
+            msg1, conv=self.conversation)
+        req = yield self.push_calls.get()
+        posted_json_data = req.content.read()
+        self.assertEqual(
+            req.requestHeaders.getRawHeaders('content-type'),
+            ['application/json; charset=utf-8'])
+        req.finish()
+        ack1 = yield event_d
+        self.assertEqual(TransportEvent.from_json(posted_json_data), ack1)
+
+    @inlineCallbacks
+    def test_post_unknown_event(self):
+        yield self.start_app_worker()
+        # temporarily pretend the worker doesn't know about acks
+        del self.app._event_handlers['ack']
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+        event_d = self.app_helper.make_dispatch_ack(
+            msg1, conv=self.conversation)
+        req = yield self.push_calls.get()
+        posted_json_data = req.content.read()
+        self.assertEqual(
+            req.requestHeaders.getRawHeaders('content-type'),
+            ['application/json; charset=utf-8'])
+        req.finish()
+        ack1 = yield event_d
+        self.assertEqual(TransportEvent.from_json(posted_json_data), ack1)
+
+    @inlineCallbacks
+    def test_post_delivery_report(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+        event_d = self.app_helper.make_dispatch_delivery_report(
+            msg1, conv=self.conversation)
+        req = yield self.push_calls.get()
+        posted_json_data = req.content.read()
+        self.assertEqual(
+            req.requestHeaders.getRawHeaders('content-type'),
+            ['application/json; charset=utf-8'])
+        req.finish()
+        ack1 = yield event_d
+        self.assertEqual(TransportEvent.from_json(posted_json_data), ack1)
+
+    @inlineCallbacks
+    def test_post_inbound_event(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+        event_d = self.app_helper.make_dispatch_ack(
+            msg1, conv=self.conversation)
+
+        req = yield self.push_calls.get()
+        posted_json_data = req.content.read()
+        self.assertEqual(
+            req.requestHeaders.getRawHeaders('content-type'),
+            ['application/json; charset=utf-8'])
+        req.finish()
+        ack1 = yield event_d
+
+        self.assertEqual(TransportEvent.from_json(posted_json_data), ack1)
+
+    @inlineCallbacks
+    def test_post_inbound_event_ignored(self):
+        yield self.start_app_worker({
+            'ignore_events': True,
+        })
+
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+        yield self.app_helper.make_dispatch_ack(
+            msg1, conv=self.conversation)
+        self.push_calls.put(None)
+        req = yield self.push_calls.get()
+        self.assertEqual(req, None)
+
+    @inlineCallbacks
+    def test_post_inbound_event_no_url(self):
+        yield self.start_app_worker({
+            'push_event_url': None,
+        })
+
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+
+        msg_prefix = 'push_event_url not configured'
+        with LogCatcher(message=msg_prefix, log_level=logging.INFO) as lc:
+            yield self.app_helper.make_dispatch_ack(
+                msg1, conv=self.conversation)
+            [url_not_configured_log] = lc.messages()
+        self.assertTrue(self.conversation in url_not_configured_log)
+
+    @inlineCallbacks
+    def test_post_inbound_event_timeout(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+
+        self._patch_http_request_full(HttpTimeoutError)
+        with LogCatcher(message='Timeout') as lc:
+            yield self.app_helper.make_dispatch_ack(
+                msg1, conv=self.conversation)
+            [timeout_log] = lc.messages()
+        self.assertTrue(timeout_log.endswith(self.mock_push_server.url))
+
+    @inlineCallbacks
+    def test_post_inbound_event_dns_lookup_error(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+
+        self._patch_http_request_full(DNSLookupError)
+        with LogCatcher(message='DNS lookup error') as lc:
+            yield self.app_helper.make_dispatch_ack(
+                msg1, conv=self.conversation)
+            [dns_log] = lc.messages()
+        self.assertTrue(self.mock_push_server.url in dns_log)
+
+    @inlineCallbacks
+    def test_post_inbound_event_connection_refused_error(self):
+        yield self.start_app_worker()
+        msg1 = yield self.make_outbound(
+            self.conversation, 'out 1', message_id='1')
+
+        self._patch_http_request_full(ConnectionRefusedError)
+        with LogCatcher(message='Connection refused') as lc:
+            yield self.app_helper.make_dispatch_ack(
+                msg1, conv=self.conversation)
+            [dns_log] = lc.messages()
+        self.assertTrue(self.mock_push_server.url in dns_log)
 
 
 class TestVumiApiWorkerWithAuth(TestVumiApiWorkerBase):
