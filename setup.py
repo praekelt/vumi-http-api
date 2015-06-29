98de3db724601503431b1a06bf46118714c1d957
Simon Cross <hodgestar@gmail.com>
2015-06-29 18:11:33 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vumi-http-api",
-    version="0.1.0",
+    version="0.1.1a",
     url='http://github.com/praekelt/vumi-http-api',
     license='BSD',
     description="An HTTP API for interacting with vumi",
--- vumi_http_api/__init__.py
+++ vumi_http_api/__init__.py
@@ -1,5 +1,5 @@
 from .vumi_api import VumiApiWorker
 
-__version__ = "0.1.0"
+__version__ = "0.1.1a"
 
 __all__ = ['VumiApiWorker']
